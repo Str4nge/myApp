679dd9e7b9f5503725560e173decf9f748eca53f
Vishal Sharma <code.amine@gmail.com>
2020-01-14 23:54:41 +0530

    Socket added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
 const port = process.env.PORT || 3333;
 
 app.use(express.static('public'));
@@ -9,7 +11,13 @@ app.get('/test',(req,res)=>{
     res.send("This is a test message!")
 });
 
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+      console.log(data);
+    });
+  });
 
-const server = app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server running on port: ${port}...`)
 });
